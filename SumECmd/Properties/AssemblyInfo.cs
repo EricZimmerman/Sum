9d684cd7f095ce519670e72efa680028e1442f47
Eric Zimmerman <saericzimmerman@gmail.com>
2021-12-12 20:56:06 -0500

    netstandard, net6

--- SumECmd/Properties/AssemblyInfo.cs
+++ SumECmd/Properties/AssemblyInfo.cs
@@ -1,18 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SumECmd")]
-[assembly: AssemblyDescription("Process Sum data from Windows Server")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Eric R. Zimmerman")]
-[assembly: AssemblyProduct("SumECmd")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("Eric R. Zimmerman")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("61eda16b-f4ca-497c-98d5-d8170e4a625c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
--- SumTest/TestMain.cs
+++ SumTest/TestMain.cs
@@ -5,156 +5,155 @@ using NLog.Targets;
 using NUnit.Framework;
 using SumData;
 
-namespace SumTest
+namespace SumTest;
+
+[TestFixture]
+public class TestMain
 {
-    [TestFixture]
-    public class TestMain
+
+    //
+
+
+    [Test]
+    public void Testing2()
     {
+        var config = new LoggingConfiguration();
+        var loglevel = LogLevel.Info;
 
-        //
+        var layout = @"${level}: ${message}";
+
+        var consoleTarget = new ColoredConsoleTarget();
+
+        config.AddTarget("console", consoleTarget);
+
+        consoleTarget.Layout = layout;
+
+        var rule1 = new LoggingRule("*", loglevel, consoleTarget);
+        config.LoggingRules.Add(rule1);
+
+        LogManager.Configuration = config;
+
+        //var r = new Srum(@"D:\OneDrive\HPSpectreSrum\Windows\System32\SRU\SRUDB.dat",@"D:\OneDrive\HPSpectreSrum\Windows\System32\config\SOFTWARE");
+
+        //  Sum.DumpTableInfo(@"C:\Temp\tout\c\Windows\System32\LogFiles\Sum\Current.mdb");
 
 
-        [Test]
-        public void Testing2()
+        var l = LogManager.GetCurrentClassLogger();
+
+        try
         {
-            var config = new LoggingConfiguration();
-            var loglevel = LogLevel.Info;
-
-            var layout = @"${level}: ${message}";
-
-            var consoleTarget = new ColoredConsoleTarget();
-
-            config.AddTarget("console", consoleTarget);
-
-            consoleTarget.Layout = layout;
-
-            var rule1 = new LoggingRule("*", loglevel, consoleTarget);
-            config.LoggingRules.Add(rule1);
-
-            LogManager.Configuration = config;
-
-            //var r = new Srum(@"D:\OneDrive\HPSpectreSrum\Windows\System32\SRU\SRUDB.dat",@"D:\OneDrive\HPSpectreSrum\Windows\System32\config\SOFTWARE");
-
-            //  Sum.DumpTableInfo(@"C:\Temp\tout\c\Windows\System32\LogFiles\Sum\Current.mdb");
-
-
-            var l = LogManager.GetCurrentClassLogger();
-
-            try
-            {
-                var r = new Sum(@"C:\Temp\DC2\c\Windows\System32\LogFiles\Sum");
+            var r = new Sum(@"C:\Temp\DC2\c\Windows\System32\LogFiles\Sum");
                 
-                Console.WriteLine();
+            Console.WriteLine();
 
-                l.Fatal("SYSTEM IDENTITY INFO");
-                foreach (var rSystemIdentityInfo in r.SystemIdentityInfos)
-                {
-                    l.Info(rSystemIdentityInfo);
-                }
-                Console.WriteLine();
-
-                l.Fatal("SYSTEM ROLE INFO");
-                foreach (var rRole in r.RoleInfos)
-                {
-                    l.Info(rRole);
-                }
-                Console.WriteLine();
-
-                l.Fatal("SYSTEM CHAINED DATABASE INFO");
-                foreach (var chained in r.ChainedDbs)
-                {
-                    l.Info(chained);
-                }
-                Console.WriteLine();
-
-                l.Fatal("PROCESSED DATABASE INFO");
-                foreach (var processed in r.ProcessedDatabases)
-                {
-                    l.Info(processed);
-                }
-
-                Console.WriteLine("____________________________-------------------------------------____________________________");
-            }
-            catch (Exception e)
+            l.Fatal("SYSTEM IDENTITY INFO");
+            foreach (var rSystemIdentityInfo in r.SystemIdentityInfos)
             {
-                Console.WriteLine(e);
-                // throw;
+                l.Info(rSystemIdentityInfo);
+            }
+            Console.WriteLine();
+
+            l.Fatal("SYSTEM ROLE INFO");
+            foreach (var rRole in r.RoleInfos)
+            {
+                l.Info(rRole);
+            }
+            Console.WriteLine();
+
+            l.Fatal("SYSTEM CHAINED DATABASE INFO");
+            foreach (var chained in r.ChainedDbs)
+            {
+                l.Info(chained);
+            }
+            Console.WriteLine();
+
+            l.Fatal("PROCESSED DATABASE INFO");
+            foreach (var processed in r.ProcessedDatabases)
+            {
+                l.Info(processed);
             }
 
+            Console.WriteLine("____________________________-------------------------------------____________________________");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            // throw;
         }
 
+    }
 
-        [Test]
-        public void Testing()
+
+    [Test]
+    public void Testing()
+    {
+        var config = new LoggingConfiguration();
+        var loglevel = LogLevel.Debug;
+
+        var layout = @"${level}: ${message}";
+
+        var consoleTarget = new ColoredConsoleTarget();
+
+        config.AddTarget("console", consoleTarget);
+
+        consoleTarget.Layout = layout;
+
+        var rule1 = new LoggingRule("*", loglevel, consoleTarget);
+        config.LoggingRules.Add(rule1);
+
+        LogManager.Configuration = config;
+
+        //var r = new Srum(@"D:\OneDrive\HPSpectreSrum\Windows\System32\SRU\SRUDB.dat",@"D:\OneDrive\HPSpectreSrum\Windows\System32\config\SOFTWARE");
+
+        //  Sum.DumpTableInfo(@"C:\Temp\tout\c\Windows\System32\LogFiles\Sum\Current.mdb");
+
+
+        var l = LogManager.GetCurrentClassLogger();
+
+        try
         {
-            var config = new LoggingConfiguration();
-            var loglevel = LogLevel.Debug;
-
-            var layout = @"${level}: ${message}";
-
-            var consoleTarget = new ColoredConsoleTarget();
-
-            config.AddTarget("console", consoleTarget);
-
-            consoleTarget.Layout = layout;
-
-            var rule1 = new LoggingRule("*", loglevel, consoleTarget);
-            config.LoggingRules.Add(rule1);
-
-            LogManager.Configuration = config;
-
-            //var r = new Srum(@"D:\OneDrive\HPSpectreSrum\Windows\System32\SRU\SRUDB.dat",@"D:\OneDrive\HPSpectreSrum\Windows\System32\config\SOFTWARE");
-
-         //  Sum.DumpTableInfo(@"C:\Temp\tout\c\Windows\System32\LogFiles\Sum\Current.mdb");
-
-
-         var l = LogManager.GetCurrentClassLogger();
-
-            try
-            {
-                var r = new Sum(@"C:\Temp\tout\c\Windows\System32\LogFiles\Sum");
+            var r = new Sum(@"C:\Temp\tout\c\Windows\System32\LogFiles\Sum");
                 
-                Console.WriteLine();
+            Console.WriteLine();
 
-                l.Fatal("SYSTEM IDENTITY INFO");
-                foreach (var rSystemIdentityInfo in r.SystemIdentityInfos)
-                {
-                    l.Info(rSystemIdentityInfo);
-                }
-                Console.WriteLine();
-
-                l.Fatal("SYSTEM ROLE INFO");
-                foreach (var rRole in r.RoleInfos)
-                {
-                    l.Info(rRole);
-                }
-                Console.WriteLine();
-
-                l.Fatal("SYSTEM CHAINED DATABASE INFO");
-                foreach (var chained in r.ChainedDbs)
-                {
-                    l.Info(chained);
-                }
-                Console.WriteLine();
-
-                l.Fatal("PROCESSED DATABASE INFO");
-                foreach (var processed in r.ProcessedDatabases)
-                {
-                    l.Info(processed);
-                }
-
-                Console.WriteLine("____________________________-------------------------------------____________________________");
-            }
-            catch (Exception e)
+            l.Fatal("SYSTEM IDENTITY INFO");
+            foreach (var rSystemIdentityInfo in r.SystemIdentityInfos)
             {
-                Console.WriteLine(e);
-               // throw;
+                l.Info(rSystemIdentityInfo);
             }
+            Console.WriteLine();
+
+            l.Fatal("SYSTEM ROLE INFO");
+            foreach (var rRole in r.RoleInfos)
+            {
+                l.Info(rRole);
+            }
+            Console.WriteLine();
+
+            l.Fatal("SYSTEM CHAINED DATABASE INFO");
+            foreach (var chained in r.ChainedDbs)
+            {
+                l.Info(chained);
+            }
+            Console.WriteLine();
+
+            l.Fatal("PROCESSED DATABASE INFO");
+            foreach (var processed in r.ProcessedDatabases)
+            {
+                l.Info(processed);
+            }
+
+            Console.WriteLine("____________________________-------------------------------------____________________________");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            // throw;
+        }
 
             
 
          
-        }
-
     }
+
 }
